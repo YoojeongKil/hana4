04e03528a3149b0d012f341870ace9cc7fd8e7b6
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-08-19 15:45:04 +0900

    tailwind

---
+++ html/tw/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#3333ff',
+        danger: '#ff3333'
+      },
+      fontSize: {
+        'big': '200px',
+      }
+    },
+  },
+  plugins: [],
+}
+
