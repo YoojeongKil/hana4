58a1e9b0c8cdfb16cd18626dcdae2c4232a0ddee
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-08-21 23:46:34 +0900

    points

--- js/hello.js
+++ js/hello.js
@@ -1,19 +1,19 @@
 var x = 1;
-console.log('Hello, World!');
+console.log("Hello, World!");
 
 let a = 1;
 let b = 2;
 
 let c = (a++, b++);
-let d = (a--, b+a);
-console.log("🚀 a, b, c, d:", a,b,c,d);
+let d = (a--, b + a);
+console.log("🚀 a, b, c, d:", a, b, c, d);
 
-d = void(c = a + b);
+d = void (c = a + b);
 console.log("🚀 d:", d);
 
 function f() {
-    console.log('xxxxxx');
-    return undefined;
+  console.log("xxxxxx");
+  return undefined;
 }
 x = f();
 console.log("🚀 x:", x);
@@ -21,11 +21,12 @@ console.log("🚀 x:", x);
 const s = `${++a}::${b}`; // {a} + '::' + {b}
 console.log("🚀 s:", s);
 
-const first = '', last = 'Bob';
-console.log(`${first}${first ? ' ' : ''}${last}`);
-console.log(`${first}${first && ' '}${last}`); // Bad(' Bob')
+const first = "",
+  last = "Bob";
+console.log(`${first}${first ? " " : ""}${last}`);
+console.log(`${first}${first && " "}${last}`); // Bad(' Bob')
 
-console.log('---------------');
+console.log("---------------");
 let aa = 0b1010;
 let bb = 0b1100;
 const and = aa & bb;
@@ -44,23 +45,22 @@ console.log("🚀 notA, notB:", notA, notB);
 console.log(aa >> 1, aa >>> 1, aa << 1);
 console.log(bb >> 1, bb >>> 1, bb << 1);
 
-
-console.log('============');
+console.log("============");
 const R = 1;
 const W = 2;
-const E = 4;   // 0b001, 0b010, 0b100
+const E = 4; // 0b001, 0b010, 0b100
 
-let auth = parseInt('011', 2);    
+let auth = parseInt("011", 2);
 console.log("🚀 auth:", auth);
-console.log('auth-bin>>', `0b0${auth.toString(2)}`);
+console.log("auth-bin>>", `0b0${auth.toString(2)}`);
 
-const hasWriteAuth = !!(auth & W);    
+const hasWriteAuth = !!(auth & W);
 console.log("🚀 hasWriteAuth:", hasWriteAuth);
-const hasExeAuth = !!(auth & E);      
+const hasExeAuth = !!(auth & E);
 console.log("🚀 hasExeAuth:", hasExeAuth);
-const hasReadAndExeAuth = !!(auth & (R | E));    
+const hasReadAndExeAuth = !!(auth & (R | E));
 console.log("🚀 hasReadAndExeAuth:", hasReadAndExeAuth);
-auth = auth ^ E;     // XOR
+auth = auth ^ E; // XOR
 console.log("🚀 auth:", auth);
 
 arr = [1, 2, 3];
--- js/loop_if.js
+++ js/loop_if.js
@@ -1,84 +1,84 @@
 var sum = 0;
 for (let i = 1; i <= 100; i += 1) {
-    sum = sum + i;
-    if(sum > 1000) break;
+  sum = sum + i;
+  if (sum > 1000) break;
 }
 console.log("🚀 for1 - sum:", sum);
 
 sum = 0;
-for (let i = 0; i < 100; sum = sum + (i += 1)) { }
+for (let i = 0; i < 100; sum = sum + (i += 1)) {}
 console.log("🚀 for2 - sum:", sum);
 
 sum = 0;
 let i = 1;
 while (i <= 100) {
-    sum += i;
-    i += 1;
+  sum += i;
+  i += 1;
 }
 console.log("🚀 while - sum:", sum);
 
 sum = 0;
 i = 0;
 do {
-    i += 1;
-    if (i % 2 === 0) continue; // 홀수만 저장
-    sum += i;
+  i += 1;
+  if (i % 2 === 0) continue; // 홀수만 저장
+  sum += i;
 } while (i < 100);
 console.log("🚀 do~while - sum:", sum);
 
 //---------
 if (sum > 0 && sum <= 10) {
-    console.log('sum=', sum);
+  console.log("sum=", sum);
 } else if (sum > 0) {
-    console.log('sum is over 10!');
+  console.log("sum is over 10!");
 } else {
-    console.log('sum is zero!');
+  console.log("sum is zero!");
 }
 
-console.log(sum > 10 ? 'T' : 'F');
+console.log(sum > 10 ? "T" : "F");
 
-const bloodType = 'A';
+const bloodType = "A";
 
-let sports = '운동';
-if (bloodType === 'A' || bloodType === 'B') {
-    sports = '근대5종';
-} else if (bloodType === 'O') {
-    sports = '배드민턴';
-} else if (bloodType === 'AB') {
-    sports = '야구';
+let sports = "운동";
+if (bloodType === "A" || bloodType === "B") {
+  sports = "근대5종";
+} else if (bloodType === "O") {
+  sports = "배드민턴";
+} else if (bloodType === "AB") {
+  sports = "야구";
 } else {
-    sports = '운동';
+  sports = "운동";
 }
-console.log("🚀 sports:", sports)
+console.log("🚀 sports:", sports);
 
 switch (bloodType) {
-    case 'A':
-    case 'B':
-        sports = '근대5종';
-        break;
-    case 'O':
-        sports = '배드민턴';
-        break;
-    case 'AB':
-        sports = '야구';
-        break;
-    default:
-        sports = '운동';
+  case "A":
+  case "B":
+    sports = "근대5종";
+    break;
+  case "O":
+    sports = "배드민턴";
+    break;
+  case "AB":
+    sports = "야구";
+    break;
+  default:
+    sports = "운동";
 }
 
-console.log("🚀 sports:", sports)
+console.log("🚀 sports:", sports);
 
-console.log('------------------');
+console.log("------------------");
 x = 30;
-let ret = x === 1 ? 'one' : x === 2 ? 'two' : x === 3 ? 'three' : 'ELSE';
-console.log('🚀  ret:', ret);
+let ret = x === 1 ? "one" : x === 2 ? "two" : x === 3 ? "three" : "ELSE";
+console.log("🚀  ret:", ret);
 
 ret =
-  (x === 1 ? 'one' : '') ||
-  (x === 2 ? 'two' : '') ||
-  (x === 3 ? 'three' : '') ||
-  'ELSE';
-console.log('🚀  ret:', ret);
+  (x === 1 ? "one" : "") ||
+  (x === 2 ? "two" : "") ||
+  (x === 3 ? "three" : "") ||
+  "ELSE";
+console.log("🚀  ret:", ret);
 
-const alpha = ['zero', 'one', 'two', 'three'];
-console.log('🚀  ret:', alpha[x] ?? 'ELSE');
+const alpha = ["zero", "one", "two", "three"];
+console.log("🚀  ret:", alpha[x] ?? "ELSE");
