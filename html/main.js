fc4859cdc5db7d25531ba1636001cbc9b2775d34
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-08-26 15:55:09 +0900

    closure

---
+++ html/main.js
@@ -0,0 +1,14 @@
+var x = 100;
+var y = 50;
+
+const p = 200;
+let q = 300;
+
+function getSum(a, b) {
+  "use strict";
+  var sum = a + b;
+  return sum;
+}
+
+var sum1 = getSum(x, y);
+var sum2 = getSum(p, q);
