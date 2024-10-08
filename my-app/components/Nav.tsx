10e39230120636c1938d51b4f287eb3aa5e3082e
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-10-08 13:09:57 +0900

    dynamic routes

---
+++ my-app/components/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <nav className='flex justify-around shadow mb-3'>
+      <a href='/'>Home</a>
+      <a href='/hello'>Hello</a>
+      <a href='/hi'>Hi</a>
+      <a href='/about'>About</a>
+    </nav>
+  );
+}
