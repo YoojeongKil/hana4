0d22fc41f470480ce439dee669ffc2f89b5eed54
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-10-11 14:24:05 +0900

    books api

--- my-app/app/photos/[photoId]/page.tsx
+++ my-app/app/photos/[photoId]/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useLayoutEffect, useState } from 'react';
+import { getPhoto, type Photo } from '@/lib/photos';
+
 export default function Photo({
   params: { photoId },
 }: {
@@ -5,5 +12,35 @@ export default function Photo({
     photoId: string;
   };
 }) {
-  return <>Photo Detail: {photoId}</>;
+  const [photo, setPhoto] = useState<Photo>();
+  useLayoutEffect(() => {
+    (async function () {
+      const data = await getPhoto(+photoId);
+      setPhoto(data);
+    })();
+  }, [photoId]);
+
+  const router = useRouter();
+  const goList = () => {
+    router.push('/photos');
+  };
+  return (
+    <>
+      <h1 className='text-2xl mt-5'>
+        #{photo?.albumId} - {photo?.title}
+      </h1>
+      {photo && (
+        <Image
+          onClick={goList}
+          src={photo.url}
+          alt={photo.title}
+          width={600}
+          height={600}
+          loading='lazy'
+          className='cursor-pointer'
+        />
+      )}
+      <h2 className='text-xl'>{photo?.title}</h2>
+    </>
+  );
 }
