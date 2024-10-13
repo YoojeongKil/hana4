996b6a3cee0f94642ceea2496d84ce3b05387aa0
Yoojeong <81125777+YoojeongKil@users.noreply.github.com>
2024-10-13 22:02:38 +0900

    next-auth

--- my-app/app/books/[bookId]/page.tsx
+++ my-app/app/books/[bookId]/page.tsx
@@ -1,6 +1,10 @@
 import { getBook } from '@/actions/books';
 import { type Book } from '@/app/api/books/bookdata';
+import { PencilIcon } from 'lucide-react';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import DelBook from '@/components/DelBook';
+import { Button } from '@/components/ui/button';
 
 export default async function Book({
   params: { bookId },
@@ -14,9 +18,32 @@ export default async function Book({
   const book = getBook(+bookId); // Good
   if (!book) return notFound();
 
+  // const remove = () => {
+  //   if (confirm('Are u sure?')) {
+  //     console.log('******');
+  //   }
+  // };
+
   return (
-    <>
-      bookId: {bookId}, {book.title}
-    </>
+    <div className='mx-3 space-y-3'>
+      <div className='flex justify-between border-b border-slate-300'>
+        bookId: <strong>{bookId}</strong>
+      </div>
+      <div className='flex justify-between border-b border-slate-300'>
+        title: <strong>{book.title}</strong>
+      </div>
+      <div className='flex justify-between border-b border-slate-300'>
+        writer: <strong>{book.writer}</strong>
+      </div>
+
+      <div className='text-right space-x-4'>
+        <DelBook id={+bookId} />
+        <Link href={`/books/${bookId}/edit`}>
+          <Button variant={'outline'}>
+            <PencilIcon /> Edit
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 }
--- my-app/app/books/page.tsx
+++ my-app/app/books/page.tsx
@@ -1,43 +1,46 @@
 'use client';
 
+import { useFetch } from '@/hooks/fetch-hook';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useLayoutEffect, useState } from 'react';
+import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { type Book } from '../api/books/bookdata';
 
 export default function Books() {
-  const [books, setBooks] = useState<Book[]>([]);
   const [searchStr, setSearchStr] = useState('');
 
-  useLayoutEffect(() => {
-    (async function () {
-      const books = (await fetch(`${process.env.BASE_URL}/api/books`).then(
-        (res) => res.json()
-      )) as Book[];
-      setBooks(books);
-    })();
-  }, []);
+  const { data: session } = useSession();
+  console.log('🚀  session:', session);
+
+  const { data: books } = useFetch<Book[]>(
+    `${process.env.NEXT_PUBLIC_URL}/api/books?q=222`
+  );
 
   return (
     <>
-      <h1 className='text-2xl'>My Books</h1>
+      <h1 className='text-xl'>{session?.user?.name} Books</h1>
 
       <Input
         onChange={(e) => setSearchStr(e.currentTarget.value)}
         placeholder='title or writer...'
       />
-      <ul className='x'>
-        {books
-          .filter(
-            ({ title, writer }) =>
-              title.includes(searchStr) || writer.includes(searchStr)
-          )
-          .map(({ id, title }) => (
-            <li key={id}>
-              <Link href={`/books/${id}`}>{title}</Link>
-            </li>
-          ))}
-      </ul>
+      {books?.length ? (
+        <ul className='x'>
+          {books
+            .filter(
+              ({ title, writer }) =>
+                title.includes(searchStr) || writer.includes(searchStr)
+            )
+            .map(({ id, title }) => (
+              <li key={id}>
+                <Link href={`/books/${id}`}>{title}</Link>
+              </li>
+            ))}
+        </ul>
+      ) : (
+        <div>There is no books.</div>
+      )}
     </>
   );
 }
